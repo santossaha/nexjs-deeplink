ee412bd0493a19e1bb30ffb864a5b8f1b90d46c2
SHS <santosh12saha@gmail.com>
2024-10-08 15:27:03 +0530

    second update

---
+++ src/app/list/page.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="container text-center">
+      <h2 className='p-5'>Current Page</h2>
+      </div>
+    </div>
+  )
+}
+
+export default page
