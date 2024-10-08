ba182708733d7a7ebd9e4955271a625f4ce48dad
SHS <santosh12saha@gmail.com>
2024-10-08 18:18:40 +0530

    update

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -17,7 +17,7 @@ function ListContent() {
 
   const handleRedirectToApp = () => {
     // Redirect the user back to the app using the deep link
-    window.location.href = 'myapp://redirect-back';
+    window.location.href = 'myapp://nexjs-deeplink-git-main-santossahas-projects.vercel.app';
   };
 
   return (
